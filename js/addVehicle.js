26c5c0009f115657f068cbea8e408b1759fc83d5
Dulatech <daviddelatycki@gmail.com>
2019-05-09 10:22:40 -0700

    added addVehicle page

--- js/addVehicle.js
+++ js/addVehicle.js
@@ -1,4 +1,6 @@
 
+
+    
     var slice = [].slice;
     var tabs = document.querySelector('.tabs');
     var tabItems = slice.call(tabs.querySelectorAll('.tabs-nav-item'));
@@ -21,3 +23,17 @@
     tabItems.forEach(function(item){
         item.addEventListener('click', onClick, false);
     });
+
+    // var e = document.getElementById("box1");
+    // var strUser = e.options[e.selectedIndex].innerHTML;
+    
+    function newPerson() {
+        $('#why').append('<div class="not" id="not"><div class="carName"><label for="year" >'+$('#box3 option:selected').text()+'</label></div><div class="gray"><label for="year" >'+$('#box2 option:selected').text()+'</label><br><label for="year" >'+$('#box1 option:selected').text()+'</label><button class="button2" onclick="removeDummy()">X </button></div></div>')
+        
+      }
+
+      function removeDummy() {
+        var elem = document.getElementById('not');
+        elem.parentNode.removeChild(elem);
+        return false;
+    }
