c8ee19b3d981483bdd0eb4bd222187a06b4e78d7
Dulatech <daviddelatycki@gmail.com>
2019-05-10 10:41:09 -0700

    added the function to prepend personal vehicles

--- js/addVehicle.js
+++ js/addVehicle.js
@@ -74,7 +74,7 @@ function selectMake() {
     // var strUser = e.options[e.selectedIndex].innerHTML;
     
     function newPerson() {
-        $('#why').append('<div class="not" id="not"><div class="carName"><label for="year" >'+$('#box3 option:selected').text()+'</label></div><div class="gray"><label for="year" >'+$('#box2 option:selected').text()+'</label><br><label for="year" >'+$('#box1 option:selected').text()+'</label><button class="button2" onclick="removeDummy()">X </button></div></div>')
+        $('#why').prepend('<div class="not" id="not"><div class="carName"><label for="year" >'+$('#box3 option:selected').text()+'</label></div><div class="gray"><label for="year" >'+$('#box2 option:selected').text()+'</label><br><label for="year" >'+$('#box1 option:selected').text()+'</label><button class="button2" onclick="removeDummy()">X </button></div></div>')
         
       }
 
