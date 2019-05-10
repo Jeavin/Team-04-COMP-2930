55bc91e8dcfa0708eee82991420cf9c2dc416cd4
sbc1142 <sbc1142@gmail.com>
2019-05-10 12:52:13 -0700

    index.js added:

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-import Navbar from 'react-bootstrap/Navbar';
+import { Navbar } from 'react-bootstrap';
+
+class App extends React.Component{
+    render(){
+        return(
+            // <>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">
+                    <img
+                         alt=""
+                        src="../images/logo.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+                    {'Emissions Planner'}
+                </Navbar.Brand>
+            </Navbar>
+            // </>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
