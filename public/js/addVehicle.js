d94c405b71dcd1e596e77065877ed1ee865ebc20
Dulatech <daviddelatycki@gmail.com>
2019-05-11 14:48:00 -0700

    deleted index.html

--- public/js/addVehicle.js
+++ public/js/addVehicle.js
@@ -1,3 +1,19 @@
+//listens for user authentication status.
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      $('.userName').html('<img class="mr-2 ml-2" src="./images/avatar.png" />'
+        + user.displayName);
+      console.log('user: ' + user.displayName);
+      console.log('uid: ' + user.uid);
+    } else {
+      $('#settingIcon1, #settingIcon2').html('<button class="btn"'
+      + 'type="button" id="signInButton" data-toggle="dropdown" '
+      + 'aria-haspopup="true" aria-expanded="false">'
+      + 'Sign In/Up</button>');
+      console.log('user: not log in');
+    }
+  });
+
 $(document).ready(function () {
     let db = firebase.database().ref().child("vehicleDatasets");
     db.on("value", snap => {
@@ -18,8 +34,8 @@ function selectYear() {
     $("#box2").empty();
     $("#box3").empty();
     // let default = $("<option disabled selected value> -- select an option -- </option>");
-    $("#box2").append($("<option disabled selected value> -- select an option -- </option>"));
-    $("#box3").append($("<option disabled selected value> -- select an option -- </option>"));
+    $("#box2").append($('<option disabled selected value="-1"> -- select an option -- </option>'));
+    $("#box3").append($('<option disabled selected value="-1"> -- select an option -- </option>'));
     bds.on("value", snap => {
         snap.forEach(function(childSnap) {
             make = childSnap.key;
@@ -35,7 +51,7 @@ function selectMake() {
     let bd = firebase.database().ref().child("vehicleDatasets/"+$('#box1').val()+"/"+$('#box2').val());
     $("#box3").empty();
     // let default = $("<option disabled selected value> -- select an option -- </option>");
-    $("#box3").append($("<option disabled selected value> -- select an option -- </option>"));
+    $("#box3").append($('<option disabled selected value="-1"> -- select an option -- </option>'));
     bd.on("value", snap => {
         snap.forEach(function(childSnap) {
             make = childSnap.key;
@@ -74,9 +90,21 @@ function selectMake() {
     // var strUser = e.options[e.selectedIndex].innerHTML;
     
     function newPerson() {
+if (document.getElementById("box1").value != -1 && document.getElementById("box2").value != -1 && document.getElementById("box3").value != -1){
+
+
         $('#why').prepend('<div class="not" id="not"><div class="carName"><label for="year" >'+$('#box3 option:selected').text()+'</label></div><div class="gray"><label for="year" >'+$('#box2 option:selected').text()+'</label><br><label for="year" >'+$('#box1 option:selected').text()+'</label><button class="button2" onclick="removeDummy()">X </button></div></div>')
+    
+    } else{
+        alert('Please Select All Options');
+    }
+}
+    
+
+
+    
         
-      }
+      
 
       function removeDummy() {
         var elem = document.getElementById('not');
